288a10791a087f5d813ce9913c46e306322882e9
Pedro Barros <pedrobars7a@gmail.com>
2021-02-27 21:18:33 -0300

    Primeiro commit

---
+++ all.js
@@ -0,0 +1,21 @@
+[
+    {
+        description: 'Luz',
+        amount: -50000,
+        date: '23/01/2021',
+    },
+    {
+        description: 'Website',
+        amount: 500000,
+        date: '23/01/2021',
+    },
+    {
+        description: 'Internet',
+        amount: -20000,
+        date: '23/01/2021'
+    },
+    {
+        description: 'App',
+        amount: 30000,
+        date: '23/01/2021'
+    }],
